be278e40473c1c0296896f61fc638e986c3115b2
HeronNepomuceno <heronnepo1@gmail.com>
2023-02-08 10:56:37 -0300

    populate endpoints with database interactions

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const sqlite = require('./server/database')
+
 const app = express()
 
 // Express servindo o front-end
@@ -6,21 +8,75 @@ app.use(express.static('./client'))
 
 // Express servindo a API
 app.get('/candidato', (request, response) => {
-    response.send('candidato works')
+  sqlite.database.all("SELECT * FROM candidato", [], (err, rows) => {
+    if (err) { throw err; }
+    let result = rows.map((row) => {
+      return {
+        id: row.id, 
+        nome: row.nome,
+        cargo: row.cargo,
+        tipo: row.tipo,
+        status: row.status
+      }
+    })
+    const json = JSON.stringify(result)
+
+    response.send(json)
+  });
 })
 
 app.get('/cargo', (request, response) => {
-    response.send('cargo works')
+  sqlite.database.all("SELECT * FROM cargo", [], (err, rows) => {
+    if (err) { throw err; }
+    let result = rows.map((row) => {
+      return {
+        id: row.id, 
+        nome: row.nome
+      }
+    })
+    const json = JSON.stringify(result)
+
+    response.send(json)
+  });
 })
 
 app.get('/municipio', (request, response) => {
-    response.send('municipio works')
+  sqlite.database.all("SELECT * FROM municipio", [], (err, rows) => {
+    if (err) { throw err; }
+    let result = rows.map((row) => {
+      return {
+        id: row.id, 
+        nome: row.nome,
+        uespi: row.uespi
+      }
+    })
+    const json = JSON.stringify(result)
+
+    response.send(json)
+  });
 })
 
 app.get('/resultadogeral', (request, response) => {
-    response.send('resultadogeral works')
+  sqlite.database.all("SELECT * FROM votacao", [], (err, rows) => {
+    if (err) { throw err; }
+    let result = rows.map((row) => {
+      return {
+        municipio: row.municipio, 
+        zona: row.zona,
+        secao: row.secao,
+        urna: row.urna,
+        candidato: row.candidato,
+        tipo: row.tipo,
+        cargo: row.cargo,
+        votos: row.votos
+      }
+    })
+    const json = JSON.stringify(result)
+
+    response.send(json)
+  });
 })
 
 app.listen(3000, () => {
-    console.log('Servidor funcionando na porta 3000')
+  console.log('Servidor funcionando em http://localhost:3000/')
 })
